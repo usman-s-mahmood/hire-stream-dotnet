e734beeec14c22d853a6a9f14b6f6bd668eddefe
Usman Shahid <usmanshahid027@outlook.com>
2025-01-14 22:43:38 +0500

    added base dotnet MVC to the project

---
+++ HireStreamDotNetProject/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+try {
+    document.getElementById('year-tag').innerHTML = new Date().getFullYear();
+} catch(error) {
+    console.error("year tag not found!");
+}
