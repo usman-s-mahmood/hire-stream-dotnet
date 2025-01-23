b772d33a97013a2ca45e082b72cfa38d98804792
Hafiz Abdul Hannan <hannanitx@gmail.com>
2025-01-23 15:55:17 -0700

    Caution! Updated User.cs model

--- HireStreamDotNetProject/Migrations/20250123225421_UpdatedUserModel.Designer.cs
+++ HireStreamDotNetProject/Migrations/20250123225421_UpdatedUserModel.Designer.cs
@@ -2,6 +2,7 @@
 using HireStreamDotNetProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HireStreamDotNetProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250123225421_UpdatedUserModel")]
+    partial class UpdatedUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
--- HireStreamDotNetProject/Models/User.cs
+++ HireStreamDotNetProject/Models/User.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HireStreamDotNetProject.Models {
     public class User {
         public int Id {get; set;}
+        [Required]
+        
+        [MinLength(8, ErrorMessage = "Username must be >= 8")]
         public string? Username { get; set; }
+        [MinLength(8, ErrorMessage = "First Name must be >= 8")]
+
         public string? FirstName { get; set; }
+        [MinLength(8, ErrorMessage = "Last Name must be >= 8")]
+
         public string? LastName { get; set; }
         public string? Email { get; set; }
         public string? Gender { get; set; }
